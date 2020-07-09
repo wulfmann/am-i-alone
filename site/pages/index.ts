0ce70d85a9586d896b34b185cbbe0ec307434b35
Joe Snell <joepsnell@gmail.com>
2020-07-08 20:38:49 -0500

    add site

---
+++ site/pages/index.ts
@@ -0,0 +1,17 @@
+import { useAlone } from 'am-i-alone';
+
+function Home() {
+  const { alone, count } = useAlone();
+  
+  const verb = count === 1 ? 'is' : 'are';
+  const suffix = count === 1 ? '' : 's';
+
+  return (
+    <div>
+      <div>{alone ? 'yes' /: 'no'}</div>
+      <div>{!alone && `There ${verb} ${count} other${suffix}}</div>
+    </div>
+  );
+};
+
+export default Home;
