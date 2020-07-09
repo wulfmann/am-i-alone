31718215a69cea4983b0a3a3989b7acb502b750f
Joe Snell <joepsnell@gmail.com>
2020-07-09 18:04:50 -0500

    typo

--- site/pages/index.ts
+++ site/pages/index.ts
@@ -5,10 +5,10 @@ function Home() {
   
   const verb = count === 1 ? 'is' : 'are';
   const suffix = count === 1 ? '' : 's';
-
+  const text = alone ? 'yes' : 'no';
   return (
     <div>
-      <div>{alone ? 'yes' : 'no'}</div>
+      <div>{text}</div>
       <div>{!alone && `There ${verb} ${count} other${suffix}}</div>
     </div>
   );
