bca132a5a256a825089abc06ad4308f9c7d09412
Joe Snell <joepsnell@gmail.com>
2020-07-09 19:04:45 -0500

    add webfomtloader

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,5 +1,15 @@
 import '../styles/global.scss';
 
+if (process.browser) {
+  import('webfontloader').then(WebFont => {
+    WebFont.load({
+      google: {
+        families: ['EB Garamond']
+      }
+    });
+  })
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
