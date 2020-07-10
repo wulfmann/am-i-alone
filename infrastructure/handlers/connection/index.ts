fa0a0672d4a597a596b34e8f752fe75412713566
Joe Snell <joepsnell@gmail.com>
2020-07-09 22:26:39 -0500

    update lambda

--- infrastructure/handlers/connection/index.ts
+++ infrastructure/handlers/connection/index.ts
@@ -1,9 +1,22 @@
-const connectHandler = (event, context) => {
+import WebSocket from 'ws';
 
+const DOMAIN = '';
+const ws = new WebSocket(DOMAIN);
+
+const connectHandler = (event, context) => {
+  const count = 2;
+  ws.send({
+    type: 'count:update',
+    data: count
+  });
 };
 
 const disconnectHandler = (event, context) => {
-
+  const count = 2;
+  ws.send({
+    type: 'count:update',
+    data: count
+  });
 };
 
 const getConnections = (event, context) => {
